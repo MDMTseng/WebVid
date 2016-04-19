2a45212faa7c881529a631e7dcc69f225d28d46d
MDM <chiapintseng@gmail.com>
2016-04-18 23:36:56 -0700

    Apply basic funcs

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-var ctx = null;
 
-function loop() {
-  if (!this.mov.paused && !this.mov.ended) {
-    this.ctx.drawImage(this.mov, 0, 0);
-    setTimeout(loop.bind(this), 1000 / 30); // drawing at 30fps
+
+var vidCanvas={
+  mov:null,
+  ctx:null,
+  canvas:null,
+  loopDraw:function() {
+    if (!this.mov.paused && !this.mov.ended) {
+      this.ctx.drawImage(this.mov,0,0);
+
+
+      var data = this.ctx.getImageData(50, 50, 1, 1).data;
+      console.log(data);
+      setTimeout(this.loopDraw.bind(this), 1000 / 3); // drawing at 30fps
+    }
   }
-}
 
+};
 
 $(function() {
   var canvas = document.getElementById('canvas');
@@ -14,10 +23,44 @@ $(function() {
   var video = document.getElementById('video');
 
   video.addEventListener('play', function() {
-    var drawPack={};
-    drawPack.mov=this;
-    drawPack.ctx=ctx;
-    console.log(drawPack);
-    loop.bind(drawPack)();
+    vidCanvas.mov=this;
+    vidCanvas.canvas=canvas;
+    vidCanvas.ctx=ctx;
+
+    canvas.width = 200;
+    canvas.height = 200;
+    console.log(vidCanvas);
+    vidCanvas.loopDraw.bind(vidCanvas)();
   }, 0);
+});
+
+
+
+$(function localFileVideoPlayer() {
+	'use strict'
+  var URL = window.URL || window.webkitURL
+  var displayMessage = function (message, isError) {
+    var element = document.querySelector('#message')
+    element.innerHTML = message
+    element.className = isError ? 'error' : 'info'
+  }
+  var playSelectedFile = function (event) {
+    var file = this.files[0]
+    var type = file.type
+    var videoNode = document.querySelector('video')
+    var canPlay = videoNode.canPlayType(type)
+    if (canPlay === '') canPlay = 'no'
+    var message = 'Can play type "' + type + '": ' + canPlay
+    var isError = canPlay === 'no'
+    displayMessage(message, isError)
+
+    if (isError) {
+      return
+    }
+
+    var fileURL = URL.createObjectURL(file)
+    videoNode.src = fileURL
+  }
+  var inputNode = document.querySelector('input')
+  inputNode.addEventListener('change', playSelectedFile, false)
 });
