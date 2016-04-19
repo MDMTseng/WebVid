2389eff618e438933349d6043fe561345decc178
MDM <chiapintseng@gmail.com>
2016-04-18 23:09:13 -0700

    Make it fancy

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+var ctx = null;
+
+function loop() {
+  if (!this.mov.paused && !this.mov.ended) {
+    this.ctx.drawImage(this.mov, 0, 0);
+    setTimeout(loop.bind(this), 1000 / 30); // drawing at 30fps
+  }
+}
+
+
 $(function() {
   var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
+  ctx=canvas.getContext('2d');
   var video = document.getElementById('video');
 
   video.addEventListener('play', function() {
-    var $this = this; //cache
-    (function loop() {
-      if (!$this.paused && !$this.ended) {
-        ctx.drawImage($this, 0, 0);
-        setTimeout(loop, 1000 / 30); // drawing at 30fps
-      }
-    })();
+    var drawPack={};
+    drawPack.mov=this;
+    drawPack.ctx=ctx;
+    console.log(drawPack);
+    loop.bind(drawPack)();
   }, 0);
 });
